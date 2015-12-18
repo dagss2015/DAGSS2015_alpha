8ee6577c1a449e889d82966282c04c69dbfe5858
Hugo Gonzalez Labrador <github@hugo.labkode.com>
2015-12-18 12:16:41 +0100

    Updated

--- src/main/java/es/uvigo/esei/dagss/controladores/medico/MedicoControlador.java
+++ src/main/java/es/uvigo/esei/dagss/controladores/medico/MedicoControlador.java
@@ -6,16 +6,22 @@ package es.uvigo.esei.dagss.controladores.medico;
 import es.uvigo.esei.dagss.controladores.autenticacion.AutenticacionControlador;
 import es.uvigo.esei.dagss.dominio.daos.CitaDAO;
 import es.uvigo.esei.dagss.dominio.daos.FarmaciaDAO;
+import es.uvigo.esei.dagss.dominio.daos.MedicamentoDAO;
 import es.uvigo.esei.dagss.dominio.daos.MedicoDAO;
+import es.uvigo.esei.dagss.dominio.daos.TratamientoDAO;
 import es.uvigo.esei.dagss.dominio.entidades.Cita;
+import es.uvigo.esei.dagss.dominio.entidades.Medicamento;
 import es.uvigo.esei.dagss.dominio.entidades.Medico;
 import es.uvigo.esei.dagss.dominio.entidades.Paciente;
 import es.uvigo.esei.dagss.dominio.entidades.Prescripcion;
 import es.uvigo.esei.dagss.dominio.entidades.Receta;
 import es.uvigo.esei.dagss.dominio.entidades.TipoUsuario;
+import es.uvigo.esei.dagss.dominio.entidades.Tratamiento;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -32,15 +38,150 @@ import javax.inject.Inject;
 @SessionScoped
 public class MedicoControlador implements Serializable {
 
+
+   
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public String getDosis() {
+        return dosis;
+    }
+
+    public void setDosis(String dosis) {
+       this.dosis = dosis;
+    }
+    
+    public List<Tratamiento> getListaDeTratamientos() {
+        return listaDeTratamientos;
+    }
+
+    public void setListaDeTratamientos(List<Tratamiento> listaDeTratamientos) {
+        this.listaDeTratamientos = listaDeTratamientos;
+    }
+    
+    // Para el medico
     private Medico medicoActual;
     private String dni;
     private String numeroColegiado;
     private String password;
+    
+    // Para el listado de tratamientos
+    List<Tratamiento> listaDeTratamientos;
+    Prescripcion prescripcionActual;
+    Tratamiento tratamientoActual;
 
+    
+    
+    Date fechaInicio;
+    Date fechaFin;
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+    String comentario;
+    String dosis;
+    
+    // Para citas
     List<Cita> citasDelMedico;
     Cita citaActual;
     
-    Prescripcion prescripcionActual;
+    // Para medicamentos
+    List<Medicamento> listaDeMedicamentos;
+    String medicamentoABuscar;
+    Medicamento medicamentoSeleccionado;
+    
+    public String doEliminarTratamiento(Tratamiento t) {
+        this.tratamientoDAO.eliminar(t);
+        this.actualizarTratamientos();
+        return "";
+    }
+    public String doCrearTratamiento() {
+        this.tratamientoActual = new Tratamiento(
+                this.citaActual.getPaciente(),
+                this.medicoActual,
+                this.comentario,
+                this.fechaInicio,
+                this.fechaFin);
+        System.out.println(this.tratamientoActual);
+        
+        this.prescripcionActual = new Prescripcion(this.comentario,
+                this.tratamientoActual, this.medicamentoSeleccionado, Integer.parseInt(this.dosis));
+        
+        List<Prescripcion> lst = new ArrayList<>();
+        lst.add(this.prescripcionActual);
+        this.tratamientoActual.setPrescipciones(lst);
+        
+        this.prescripcionActual.setTratamiento(this.tratamientoActual);
+        
+        this.tratamientoDAO.crear(this.tratamientoActual);
+        
+        this.actualizarTratamientos();
+        
+        return "";
+    }
+
+    public String doShowFormularioTratamiento(Medicamento m) {
+        this.medicamentoSeleccionado = m;
+        return "";
+    }
+    public String doBuscarMedicamento() {
+        List<Medicamento> lst;
+        lst = medicamentoDAO.buscarPorNombre(this.medicamentoABuscar);
+        this.listaDeMedicamentos = lst;
+        System.err.println(lst);
+        return "";
+    }
+    public List<Medicamento> getListaDeMedicamentos() {
+        return listaDeMedicamentos;
+    }
+
+    public void setListaDeMedicamentos(List<Medicamento> listaDeMedicamentos) {
+        this.listaDeMedicamentos = listaDeMedicamentos;
+    }
+
+    public String getMedicamentoABuscar() {
+        return medicamentoABuscar;
+    }
+
+    public void setMedicamentoABuscar(String medicamentoABuscar) {
+        this.medicamentoABuscar = medicamentoABuscar;
+    }
+
+    public Medicamento getMedicamentoSeleccionado() {
+        return medicamentoSeleccionado;
+    }
+
+    public void setMedicamentoSeleccionado(Medicamento medicamentoSeleccionado) {
+        this.medicamentoSeleccionado = medicamentoSeleccionado;
+    }
+
+    public Tratamiento getTratamientoActual() {
+        return tratamientoActual;
+    }
+
+    public void setTratamientoActual(Tratamiento tratamientoActual) {
+        this.tratamientoActual = tratamientoActual;
+    }
+    
+   
     
     public Prescripcion getPrescripcionActual() {
         return prescripcionActual;
@@ -78,9 +219,11 @@ public class MedicoControlador implements Serializable {
 
     @EJB
     private MedicoDAO medicoDAO;
-    
+    @EJB
+    private MedicamentoDAO medicamentoDAO;
     @EJB
     private CitaDAO citaDAO;
+    @EJB TratamientoDAO tratamientoDAO;
 
     /**
      * Creates a new instance of AdministradorControlador
@@ -160,6 +303,11 @@ public class MedicoControlador implements Serializable {
     //Acciones
     public String doShowCita(Cita c) {
         this.citaActual = c;
+        this.actualizarTratamientos();
         return "detallesCita";
+    }
+    
+    private void actualizarTratamientos() {
+        this.listaDeTratamientos = this.tratamientoDAO.buscarTodos();
     }
 }
--- src/main/java/es/uvigo/esei/dagss/dominio/daos/MedicamentoDAO.java
+++ src/main/java/es/uvigo/esei/dagss/dominio/daos/MedicamentoDAO.java
@@ -4,12 +4,21 @@
 package es.uvigo.esei.dagss.dominio.daos;
 
 import es.uvigo.esei.dagss.dominio.entidades.Medicamento;
+import java.util.List;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.persistence.Query;
 
 @Stateless
 @LocalBean
 public class MedicamentoDAO extends GenericoDAO<Medicamento> {
 
     // Completar aqui
+    public List<Medicamento> buscarPorNombre(String nombre) {
+        Query q = em.createQuery("SELECT m FROM Medicamento AS m "
+                + "  WHERE m.nombre LIKE :nombre");
+        q.setParameter("nombre","%"+ nombre+ "%");
+
+        return q.getResultList();
+    }
 }
